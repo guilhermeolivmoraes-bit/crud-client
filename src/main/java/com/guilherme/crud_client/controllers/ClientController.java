f44f8d05d5fcf87445c4d6e163c70e39f9293304
guilhermeolivmoraes-bit <guilherme.olivmoraes@gmail.com>
2026-06-07 16:38:32 -0300

    Control Class and Routs

---
+++ src/main/java/com/guilherme/crud_client/controllers/ClientController.java
@@ -0,0 +1,17 @@
+package com.guilherme.crud_client.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/clients")
+public class ClientController {
+
+    @GetMapping
+    public String teste(){
+        return "Olá Mundo!";
+    }
+
+
+}
