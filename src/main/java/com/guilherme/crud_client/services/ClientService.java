aacc9479bba853cb86fd7a5a4e032281c03cba3c
guilhermeolivmoraes-bit <guilherme.olivmoraes@gmail.com>
2026-06-10 12:23:09 -0300

    GET Clients paged

--- src/main/java/com/guilherme/crud_client/services/ClientService.java
+++ src/main/java/com/guilherme/crud_client/services/ClientService.java
@@ -4,8 +4,12 @@ import com.guilherme.crud_client.dto.ClientDTO;
 import com.guilherme.crud_client.entities.Client;
 import com.guilherme.crud_client.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class ClientService {
@@ -17,5 +21,11 @@ public class ClientService {
     public ClientDTO findById(Long id) {
             Client client = repository.findById(id).get();
             return new ClientDTO(client);
+    }
+
+    @Transactional
+    public Page<ClientDTO> findAll(Pageable pageable) {
+        Page<Client> result = repository.findAll(pageable);
+        return result.map(x -> new ClientDTO(x));
     }
 }
